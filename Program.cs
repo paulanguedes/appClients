dbf456eb371c1d74f19d95cad41e369d7aa430c4
paulanguedes <paulanguedes@gmail.com>
2023-09-09 12:14:03 -0300

    :construction: create Menu method

--- Program.cs
+++ Program.cs
@@ -6,4 +6,19 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("CLIENT REGISTER OPTIONS");
+        Console.WriteLine("==========================");
+        Console.WriteLine("1 - Client Register");
+        Console.WriteLine("2 - Show clients");
+        Console.WriteLine("3 - Update client");
+        Console.WriteLine("4 - Delete client");
+        Console.WriteLine("5 - Exit");
+        Console.WriteLine("==========================");
+
+
+    }
 }
