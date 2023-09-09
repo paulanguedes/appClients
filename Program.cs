eb6717f4640386eaea85e3f5afb90f96dfddd576
paulanguedes <paulanguedes@gmail.com>
2023-09-09 12:17:27 -0300

    :construction: update Main with the Menu method

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while(true)
+        {
+            Menu();
+            Console.ReadKey();
+        }
     }
 
     static void Menu()
