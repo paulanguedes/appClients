49cb92c65b682461c21ad960053f3c3bac68877a
paulanguedes <paulanguedes@gmail.com>
2023-09-09 13:06:04 -0300

    :rewind: change client's ID type to int

--- Client.cs
+++ Client.cs
@@ -2,7 +2,7 @@ namespace Cadastro;
 
 public class Client
 {
-    public Guid Id { get; set; }
+    public int Id { get; set; }
     public string Name { get; set; }
     public DateOnly Birth { get; set; }
     public DateTime RegisteredIn { get; set; }
