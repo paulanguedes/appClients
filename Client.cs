cdca31cd3dd0c6cfd7a64e309587b6e94425ab93
paulanguedes <paulanguedes@gmail.com>
2023-09-09 13:03:42 -0300

    :mute: removing warning logs

--- Client.cs
+++ Client.cs
@@ -2,8 +2,8 @@ namespace Cadastro;
 
 public class Client
 {
-    public int Id { get; set; }
-    public string? Name { get; set; }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
     public DateOnly Birth { get; set; }
     public DateTime RegisteredIn { get; set; }
     public decimal Discount { get; set; }
