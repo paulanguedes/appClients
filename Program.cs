ae5df7557016b3da3df3f21b7feaa3063a1d8d58
paulanguedes <paulanguedes@gmail.com>
2023-09-09 13:17:33 -0300

    :construction: create ChooseOption method

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿namespace AppClients;
+﻿using Repository;
+
+namespace AppClients;
 
 class Program
 {
+    private static ClientRepository _clientRepository = new ClientRepository();
     static void Main(string[] args)
     {
         while(true)
@@ -22,7 +25,50 @@ class Program
         Console.WriteLine("4 - Delete client");
         Console.WriteLine("5 - Exit");
         Console.WriteLine("==========================");
+    }
 
+    static void ChooseOption()
+    {
+        Console.WriteLine("Choose an option: ");
+        var option = int.Parse(Console.ReadLine());
 
+        switch (option)
+        {
+            case 1:
+            {
+                _clientRepository.ClientRegister();
+                Menu();
+                break;
+            }
+            case 2:
+            {
+                _clientRepository.ShowClients();
+                Menu();
+                break;
+            }
+            case 3:
+            {
+                _clientRepository.UpdateClient();
+                Menu();
+                break;
+            }
+            case 4:
+            {
+                _clientRepository.DeleteClient();
+                Menu();
+                break;
+            }
+            case 5:
+            {
+                Environment.Exit(0);
+                break;
+            }
+            default:
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid option!");
+                break;
+            }
+        }
     }
 }
